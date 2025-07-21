1bc0559b8fcce94c295a3f050adc741161de5397
Atul pandey <atulpandey0412@gmail.com>
2025-07-21 14:29:51 +0530

    update the mai.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthProvider.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AuthProvider>
